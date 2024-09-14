6adca7e52f35c84561a9123538e9307d6722d9b6
BeastieNate5 <nhagan345@gmail.com>
2024-09-14 03:00:54 -0400

    Finished getting hash into memory

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include<stdlib.h>
 #include<stdbool.h>
 #include<sys/stat.h>
+#include<string.h>
 
 #define MAX_HASH_SIZE = 4096
 
@@ -44,8 +45,18 @@ long get_filesize(FILE* fp) {
     }
 
     long size = ftell(fp);
-
+    fseek(fp, 0, SEEK_SET);
     return size;
+}
+
+char* allocate_hash(long size) {
+    char* allocatedP = (char*)malloc(size);
+
+    if (allocatedP == NULL) {
+        return NULL;
+    }
+
+    return allocatedP;
 }
 
 int main(int argc, char* argv[]) {
@@ -56,10 +67,13 @@ int main(int argc, char* argv[]) {
 
     char* hash_location = argv[1];
     char* wordlist_location = argv[2];
+    FILE* hash_file;
+    bool isFile = false;
     
     int hash_size;
     if (check_if_file_exist(hash_location)) {
-        FILE* hash_file = fopen(hash_location, "r");
+        hash_file = fopen(hash_location, "r");
+        isFile = true;
         
         if (hash_file == NULL) {
             report_log(1, "Error opening file");
@@ -67,13 +81,37 @@ int main(int argc, char* argv[]) {
         }
 
         hash_size = get_filesize(hash_file);
+        printf("\x1b[92m[+]\x1b[0m Located Hash file <%ld bytes>\n", hash_size);
     } 
     // If we can not find a file just treat the arguement as the actual hash
     else {
-       hash_size = sizeof(hash_location);
+       hash_size = strlen(hash_location);
+       printf("\x1b[92m[+]\x1b[0m Hash size <%ld bytes>\n", hash_size);
+    }
+
+    // For null term
+    hash_size += 1;
+    char* hash = allocate_hash(hash_size);
+
+    if (hash != NULL) {
+        printf("\x1b[92m[+]\x1b[0m Successfully allocated <%ld bytes>\n", hash_size);
+    } else {
+        report_log(1, "Failed to allocate space for hash");
+    }
+
+    if (isFile) {
+        if (fgets(hash, hash_size, hash_file) == NULL) {
+            printf("\x1b[91m[-]\x1b[0m Failed to read contents of hash file\n");
+            exit(1);
+        }
+    } else {
+        strcpy(hash, hash_location);
     }
     
-   printf("\x1b[92m[+]\x1b[0m Located Hash file <%ld bytes>\n", hash_size); 
+    printf("\x1b[92m[+]\x1b[0m Wrote hash to allocated memory\n");
+
+    printf("Hash: %s\n", hash);
+    free(hash);
 
 }
 
